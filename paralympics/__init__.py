c21dd157e281b6a469a9c128ff7aeade070d7b8a
nicholsons <sarah.sanders@ucl.ac.uk>
2024-02-13 08:27:12 +0000

    Changed conftest.py to close the database before deleting

--- paralympics/__init__.py
+++ paralympics/__init__.py
@@ -27,6 +27,7 @@ def create_app(test_config=None):
         SECRET_KEY='l-tirPCf1S44mWAGoWqWlA',
         # configure the SQLite database, relative to the app instance folder
         SQLALCHEMY_DATABASE_URI="sqlite:///" + os.path.join(app.instance_path, 'paralympics.sqlite'),
+        SQLALCHEMY_ECHO = True,
     )
 
     if test_config is None:
--- tests/conftest.py
+++ tests/conftest.py
@@ -33,6 +33,11 @@ def app():
     # clean up / reset resources
     # Delete the test database (if adding data to your database takes a long time you may prefer not to delete the
     # database)
+    with app.app_context():
+        # Close the database session
+        db.session.close()
+        db.session.remove()
+        db.engine.dispose()
     os.unlink(db_path)
 
 
